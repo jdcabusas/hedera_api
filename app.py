28d088c6979f4aa76aa638e6e7f87fb9166bd69e
Cabusas, Joseph <jdcabusas201@gmail.com>
2024-10-30 11:01:08 -0400

    Opening external IPs in app.py

--- app.py
+++ app.py
@@ -78,4 +78,4 @@ def get_topic_ids():
         return jsonify({'success': False, 'message': 'An error occurred.', 'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
